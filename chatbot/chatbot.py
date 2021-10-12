d0c9ab03ad5eb99de88b70d83571129dfbb7a4d4
Gustavo Scholze <scholzegustavo@gmail.com>
2021-10-12 08:56:27 -0300

    Luong attention layer ready

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -401,7 +401,15 @@ class EncoderRNN(nn.Module):
 class Attn(nn.Module):
     def __init__(self, method, hidden_size):
         super(Attn, self).__init__()
-        pass
+        self.method = method
+        if self.method not in ['dot', 'general', 'concat']:
+            raise ValueError(self.method, 'não é um método de atenção legítimo!')
+        self.hidden_size = hidden_size
+        if self.method == 'general':
+            self.attn = nn.Linear(self.hidden_size, hidden_size)
+        elif self.method == 'concat':
+            self.attn = nn.Linear(self.hidden_size*2, hidden_size)
+            self.v = nn.Parameter(torch.FloatTensor(hidden_size))
 
     def dot_score(self, hidden, encoder_output):
         return torch.sum(hidden * encoder_output, dim=2)
@@ -410,11 +418,23 @@ class Attn(nn.Module):
         energy = self.attn(encoder_output)
         return torch.sum(hidden * energy, dim=2)
 
-    def concat_score(self):
-        pass
+    def concat_score(self, hidden, encoder_output):
+        energy = self.attn(torch.cat((hidden.expand(encoder_output.size(0), -1, -1),
+                                      encoder_output), 2)).tanh()
+        return torch.sum(self.v * energy, dim=2)
 
-    def forward(self):
-        pass
+    def forward(self, hidden, encoder_outputs):
+        # Calcula os pesos da atenção (energia) baseado no método
+        if self.method == 'general':
+            attn_energies = self.general_score(hidden, encoder_outputs)
+        elif self.method == 'concat':
+            attn_energies = self.concat_score(hidden, encoder_outputs)
+        elif self.method == 'dot':
+            attn_energies = self.dot_score(hidden, encoder_outputs)
 
-    return 0
+        attn_energies = attn_energies.t()
+
+        return F.softmax(attn_energies, dim=1).unsqueeze(1)
+
+
 
