a96c88b13fd72026aed2a77b1e2262e8478a1de1
Gustavo Scholze <scholzegustavo@gmail.com>
2021-10-12 09:36:33 -0300

    Defined masked loss

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -483,3 +483,37 @@ class LuongAttnDecoderRNN(nn.Module):
 
         # Retorna output e o estado final
         return output, hidden
+
+
+# Definição dos procedimentos para treino
+def maskNLLLoss(inp, target, mask):
+    nTotal = mask.sum()
+    crossEntropy = -torch.log(torch.gather(inp, 1, target.view(-1,1)).squeeze(1))
+    loss = crossEntropy.masked_select(mask).mean()
+    loss = loss.to(DEVICE)
+    return loss, nTotal.item()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
